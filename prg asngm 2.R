b580f73c66c3c46cbb58761fb4492f0246e1e9b3
manikiran003 <65855838+manikiran003@users.noreply.github.com>
2020-08-23 19:32:58 +0530

    Add files via upload

---
+++ prg asngm 2.R
@@ -0,0 +1,30 @@
+makeCacheMatrix <- function(x = matrix()) {
+  
+  xinv <- NULL
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) xinv <<- inv
+  getinv <- function() xinv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+
+
+
+cacheSolve <- function(x, ...) {
+  xinv <- x$getinv()
+  if(!is.null(xinv)) {
+    message("getting cached data")
+    return(xinv)
+  }
+  data <- x$get()
+  xinv <- solve(data, ...)
+  x$setinv(xinv)
+  xinv
+}
